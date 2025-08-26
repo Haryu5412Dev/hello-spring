53c33d60aaa353efd37b18aedb75b492a1619ba7
Haryu5412Dev <haryu5412@gbsw.gyo6.net>
2025-08-26 10:53:42 +0900

    테스트 서버 포트 지정과 외부 접속 막기

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,11 +3,21 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		// SpringApplication.run(HelloSpringApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(HelloSpringApplication.class, args);
+
+		Environment env = context.getEnvironment();
+		String port = env.getProperty("server.port", "8080");
+		String path = env.getProperty("server.servlet.context-path", "");
+
+		System.out.println("서버 실행 경로: http://localhost:" + port + path + "/");
 	}
 
 }
