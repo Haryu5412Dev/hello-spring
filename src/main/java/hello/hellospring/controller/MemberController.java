8d5264db70975525f818bc67ff9b21d6324248ff
Haryu5412 <126925788+Haryu5412Dev@users.noreply.github.com>
2025-10-27 16:01:04 +0900

    버그 수정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -15,4 +15,13 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        
+        return "redirect:/";
+    }
 }
